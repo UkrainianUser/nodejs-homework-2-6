ed6fb9e332d417237d0b2353320fb3e30dd938bf
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-07-02 19:37:26 +0300

    server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const app = require("./app");
 const { DB_HOST } = process.env;
 
+mongoose.set("strictQuery", true);
+
 mongoose
 	.connect(DB_HOST)
 	.then(() => {
