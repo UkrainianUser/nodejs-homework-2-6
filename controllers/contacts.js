85f0664743ff60ef713a610f2cd0d3e328f58473
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-07-02 23:48:51 +0300

    add updateStatusContact

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -71,10 +71,29 @@ const changeContact = async (req, res, next) => {
 	}
 };
 
+const updateStatusContact = async (req, res, next) => {
+	try {
+		const { contactId } = req.params;
+		const favorite = req.body;
+		const result = await Contact.findByIdAndUpdate(
+			contactId,
+			favorite,
+			req.body
+		);
+		if (!result) {
+			throw HttpError(404, "missing field favorite");
+		}
+		res.json(result);
+	} catch (error) {
+		next(error);
+	}
+};
+
 module.exports = {
 	getAllContacts,
 	getById,
 	addContact,
 	deleteContact,
 	changeContact,
+	updateStatusContact,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -14,4 +14,6 @@ router.delete("/:contactId", controllers.deleteContact);
 
 router.put("/:contactId", controllers.changeContact);
 
+router.patch("/:contactId/favorite", controllers.updateStatusContact);
+
 module.exports = router;
