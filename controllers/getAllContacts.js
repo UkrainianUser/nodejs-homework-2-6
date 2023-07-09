a951a54af33296431032648c4dcbacbd4f4adaf2
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-07-09 23:20:41 +0300

    Update getAllContacts.js

--- controllers/getAllContacts.js
+++ controllers/getAllContacts.js
@@ -1,8 +1,7 @@
-const Contact = require("../models");
+const { Contact } = require("../models");
 
 const getAllContacts = async (req, res, next) => {
 	try {
-		console.log(req);
 		const result = await Contact.find();
 		res.json(result);
 	} catch (error) {
