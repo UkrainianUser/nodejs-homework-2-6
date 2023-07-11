26a5875315782393011fee7719ce56c3aba5fa54
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-07-11 21:45:49 +0300

    Update addContact.js

--- controllers/addContact.js
+++ controllers/addContact.js
@@ -7,7 +7,7 @@ const addContact = async (req, res, next) => {
 		const { error } = addSchema.validate(req.body);
 		console.log(error);
 		if (error) {
-			throw new HttpError(400, "missing required name field");
+			throw HttpError(400, "missing required name field");
 		}
 		const { _id: owner } = req.user;
 		const result = await Contact.create({ ...req.body, owner });
