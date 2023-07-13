dba1b7c6d68aed30100c1120ef9564b45a6cfda4
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-07-13 19:53:29 +0300

    Update updateAvatar.js

--- controllers/updateAvatar.js
+++ controllers/updateAvatar.js
@@ -6,10 +6,10 @@ const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
 	const { _id } = req.user;
-	const { path, tempUpload, originalname } = req.file;
+	const { path: tempUpload, originalname } = req.file;
 	const resultUpload = path.join(avatarsDir, originalname);
 	await fs.rename(tempUpload, resultUpload);
-	const avatarURL = path.join("avatar", originalname);
+	const avatarURL = path.join("avatars", originalname);
 	await User.findByIdAndUpdate(_id, { avatarURL });
 
 	res.json({
