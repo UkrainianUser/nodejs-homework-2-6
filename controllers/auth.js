d7da462104cfc6697ff8ff277dc676b1c7039ab5
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-07-23 15:28:54 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,7 +13,7 @@ const register = async (req, res) => {
 	const { email, password } = req.body;
 	const user = await User.findOne({ email });
 
-	if (!user) {
+	if (user) {
 		throw HttpError(409, "Email in use");
 	}
 
